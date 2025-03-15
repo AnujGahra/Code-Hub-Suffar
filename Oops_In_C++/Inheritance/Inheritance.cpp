3ab487eec286bc101650b61e95db0bbcec0a30a7
AnujGahra <anujgahra244304@gmail.com>
2025-03-15 23:37:05 +0530

    Access Modifier

--- Oops_In_C++/Inheritance/Inheritance.cpp
+++ Oops_In_C++/Inheritance/Inheritance.cpp
@@ -1,2 +1,28 @@
 // Inheritance
 // The capability of a class to derive property and characteristic from another class. 
+
+#include<iostream>
+using namespace std;
+
+
+class Human {
+    private:
+    int a;
+    protected:
+    int b;
+    public:
+    int c;
+
+
+    void fun(){
+        a = 10;
+        b = 20;
+        c = 30;
+    }
+};
+
+int main(){
+    Human Anuj;
+    // Anuj.c = 10;
+    Anuj.fun();
+};
