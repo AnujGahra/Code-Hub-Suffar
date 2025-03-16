5ede5f1035aa97ab82824981bb697bf05c59e1ed
AnujGahra <anujgahra244304@gmail.com>
2025-03-16 22:49:55 +0530

    Single Inheritance

---
+++ Oops_In_C++/TypesOfInheritance/intro.cpp
@@ -0,0 +1,34 @@
+// Single Inheritance
+#include<iostream>
+using namespace std;
+
+
+class Human{
+    protected:
+    string name;
+    int age;
+
+    public:
+
+    void work()
+    {
+        cout << "I am working \n";
+    }
+};
+
+
+class Student: public Human {
+    int roll_number, fees;
+
+    public:
+    Student(string name, int age, int roll_number, int fees){
+        this->name = name;
+        this->age = age;
+        this->roll_number = roll_number;
+        this->fees = fees;
+    }
+};
+
+int main(){
+    Student A1("Anuj", 23, 32, 99);
+};
